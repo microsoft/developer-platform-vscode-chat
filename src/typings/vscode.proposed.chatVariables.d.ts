b75237fac28b4e67985ed8bb02c9ddba88b7e347
Colby Williams <ColbyLWilliams@gmail.com>
2023-12-07 15:34:09 -0600

    auto generate the proposal apis (typings) (#5)

--- src/typings/vscode.proposed.chatVariables.d.ts
+++ src/typings/vscode.proposed.chatVariables.d.ts
@@ -1,38 +1,39 @@
-// /*---------------------------------------------------------------------------------------------
-//  *  Copyright (c) Microsoft Corporation. All rights reserved.
-//  *  Licensed under the MIT License. See License.txt in the project root for license information.
-//  *--------------------------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 
 declare module 'vscode' {
-    export interface InteractiveRequest {
-        variables: Record<string, ChatVariableValue[]>;
-    }
 
-    export enum ChatVariableLevel {
-        Short = 1,
-        Medium = 2,
-        Full = 3
-    }
+	export interface InteractiveRequest {
+		variables: Record<string, ChatVariableValue[]>;
+	}
 
-    export interface ChatVariableValue {
-        level: ChatVariableLevel;
-        value: string;
-        description?: string;
-    }
+	export enum ChatVariableLevel {
+		Short = 1,
+		Medium = 2,
+		Full = 3
+	}
 
-    export interface ChatVariableContext {
-        message: string;
-    }
+	export interface ChatVariableValue {
+		level: ChatVariableLevel;
+		/**
+		 * An optional type tag for extensions to communicate the kind of the variable. An extension might use it to interpret the shape of `value`.
+		 */
+		kind?: string;
+		value: string | Uri | any;
+		description?: string;
+	}
 
-    export interface ChatVariableResolver {
-        resolve(
-            name: string,
-            context: ChatVariableContext,
-            token: CancellationToken
-        ): ProviderResult<ChatVariableValue[]>;
-    }
+	export interface ChatVariableContext {
+		message: string;
+	}
 
-    export namespace chat {
-        export function registerVariable(name: string, description: string, resolver: ChatVariableResolver): Disposable;
-    }
+	export interface ChatVariableResolver {
+		resolve(name: string, context: ChatVariableContext, token: CancellationToken): ProviderResult<ChatVariableValue[]>;
+	}
+
+	export namespace chat {
+		export function registerVariable(name: string, description: string, resolver: ChatVariableResolver): Disposable;
+	}
 }
